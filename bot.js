f31f24732f9a77aff6992301503d62fa20f592ab
Mona Lisa <evgeniy8705@gmail.com>
2020-11-07 13:24:53 +0600

    fix

--- bot.js
+++ bot.js
@@ -1,49 +1,63 @@
 const TelegramBot = require("node-telegram-bot-api");
 const fetch = require("node-fetch");
 
-const token = "1423055464:AAH-3QX7eKq_eSAxRJ9VhPLRj1Lumq-INXQ";
+const token = "1423055464:AAGkEeD8xwKcuWZg4hZ68Ko1MgGiK7V0MBs";
 const bot = new TelegramBot(token, {
   polling: true,
   webHook: process.env.PORT,
 });
 
-bot.on("message", async (msg) => {
-  let interval = setInterval(async () => {
-    let response = await fetch("https://api.binance.com/api/v3/ticker/price");
+const necessarySymbols = ["btcusdt", "ethusdt", "bnbusdt"];
 
-    if (response.ok) {
-      let json = await response.json();
+const getSymbolsData = async () => {
+  let response = await fetch("https://api.binance.com/api/v3/ticker/price");
 
-      let symbols = json.filter(symbol => {
-        if (symbol.symbol == "ETHUSDT" || symbol.symbol == "BTCUSDT" || symbol.symbol == "BNBUSDT") {
-          return true;
-        }
-      });
+  if (response.ok) return response.json();
+  else throw Error("Response is not OK");
+};
 
-      let date = new Date();
-      let options = new Intl.DateTimeFormat("ru", {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        timezone: 'UTC',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric'
-      });
+const getFilteredSymbols = (allSymbols, filter) => {
+  let filteredSymbols = [];
 
-      let html = `
+  filter.forEach(symbol => {
+    allSymbols.forEach(allSymbolsItem => {
+      if (allSymbolsItem.symbol.toUppercase() == symbol.toUppercase()) {
+        filteredSymbols.push(allSymbolsItem);
+      }
+    });
+  });
+
+  return filteredSymbols;
+};
+
+const getHTMLMessage = symbols => {
+  let htmlMessage = `
 <b>${symbols[0].symbol}</b> - <code>${(+symbols[0].price).toFixed(4)}$</code>
 <b>${symbols[1].symbol}</b> - <code>${(+symbols[1].price).toFixed(4)}$</code>
 <b>${symbols[2].symbol}</b> - <code>${(+symbols[2].price).toFixed(4)}$</code>
       `;
 
-      if (msg.chat.id) {
-        bot.sendMessage(msg.chat.id, html, {
-          "parse_mode": "HTML"
-        });
-      }
-    } else {
-      console.log("error")
+    return htmlMessage;
+};
+
+
+bot.on("message", msg => {
+  let interval = setInterval(async () => {
+    try {
+      let allSymbols = getSymbolsData();
+    } catch (err) {
+      console.log(err)
     }
-  }, 30000);
+
+    let filteredSymbols = getFilteredSymbols(allSymbols, necessarySymbols);
+    let htmlMessage = getHTMLMessage();
+
+    if (msg.chat.id) {
+      bot.sendMessage(msg.chat.id, htmlMessage, {
+        "parse_mode": "HTML"
+      });
+    } else {
+      clearInterval(interval);
+    }
+  }, 5000);
 });
